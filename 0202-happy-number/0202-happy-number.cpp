729af7b5c6473591bd37aeaa1bf386b010f8bd09
Sumeet <sumeetattri09@gmail.com>
2026-08-30 16:01:50 +0530

    Time: 0 ms (100%), Space: 8.3 MB (50.23%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        map<int,int>mp;
+        while(n!=1){
+            if(mp.find(n)!=mp.end()){
+                return false;
+            }
+            mp[n]++;
+            
+            int sum=0;
+            while(n){
+                int x=n%10;
+                sum=sum+x*x;
+                n=n/10;
+            }
+            n=sum;
+        }
+        return true;
+    }
+};
